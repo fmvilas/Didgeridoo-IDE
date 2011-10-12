3a57704772b090bfdb05910e04bfc10077bf6e98
fmvilas <fmvilas@gmail.com>
2011-10-12 14:54:00 +0200

    Solved problem with Web Workers in Ace editor
    Actually it's not solved, it's only deactivated the use of Web Workers
    with Ace

--- app/modules/ui/ace/init.js
+++ app/modules/ui/ace/init.js
@@ -2,4 +2,5 @@ var editor = ace.edit("didgeridoo-code-editor-ace");
 editor.setTheme("ace/theme/vibrant_ink");
 
 var JavaScriptMode = require("ace/mode/javascript").Mode;
+editor.getSession().setUseWorker(false);
 editor.getSession().setMode(new JavaScriptMode());
