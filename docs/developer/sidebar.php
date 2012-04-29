7541b93d1d8570aaad053666b68744d369a8f4c7
fmvilas <fmvilas@gmail.com>
2012-04-29 22:24:57 +0200

    Docs Structure fixes

--- docs/developer/sidebar.php
+++ docs/developer/sidebar.php
@@ -1,7 +1,7 @@
 				<div class="span3">
 					<div class="well sidebar-nav">
 						<ul class="nav nav-list">
-							<li class="active">
+							<li>
 								<a href="index.php">Overview</a>
 							</li>
 							<li class="nav-header">
