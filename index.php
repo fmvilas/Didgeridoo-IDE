ffe89699144f9117529da09fee3a16ad6fc60f4c
fmvilas <fmvilas@gmail.com>
2012-08-13 13:46:17 +0200

    Removing testing files...

--- index.php
+++ index.php
@@ -1,26 +1,19 @@
-<?php
-	/*
-	
-	//declare(encoding='UTF-8');
-	
-	//Global namespace
-	namespace {
-		
-		//REST Server (Slim)
-		require 'app/server/rest/Slim.php';
-		
-		$rest = new Slim();
-		
-		$rest->map('/', function() {
-			echo 'hola';
-		})->via('GET, POST');
-		
-		//$rest->run();
-		
-		echo 'everything is fine';
-		
-	}
-	*/
-	require 'app/server/rest/index.php';
-	
-?>
+<!DOCTYPE html>
+<html>
+    <head>
+    
+    	<meta http-equiv="Cache-control" content="no-cache">
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+        <title>Didgeridoo IDE</title>
+        
+        <link rel="stylesheet" type="text/css" href="app/modules/ui/_css/common.css" />
+        
+        <link rel="icon" href="img/icon.png" />
+                
+        <script src="app/libraries/require/require.js"></script>
+        <script src="app/init/init.js"></script>
+        
+    </head>
+    <body>
+    </body>
+</html>
